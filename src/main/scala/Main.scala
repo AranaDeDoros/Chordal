78547d4965488627232c2b815e2e8310ea8e387e
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-12-25 22:40:07 -0600

    main with examples

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,10 @@
 package org.aranadedoros.chordal
 
-import notes.note
+import notes.{note, sharpen}
 import intervals.*
 import chords.*
 import progressions.*
+import extensions.{FlatNinth, SharpEleventh}
 
 object Main:
   def main(args: Array[String]): Unit =
@@ -12,15 +13,14 @@ object Main:
     val third = root.transposeBy(ThirdMajorInterval)
     val fifth = root.transposeBy(FifthPerfectInterval)
     // let's create the chord
-    val chord = Triad(root, third, fifth)
+    val chord = Triad(root, MajorChord)
     println(chord)
     // but we're playing rock, let's drop that third and gets us a
     val powerChord = chord.toPowerChord
     println(powerChord)
-    // let's add a seventh
-    val seventhMinor = root.transposeBy(SeventhMinorInterval)
-    val seventhDom   = chord.addExtension(seventhMinor)
-    println(s"$seventhDom") // will find out how to work with enharmonics later
+    // let's add an extension
+    val flatNinth = chord.addExtension(FlatNinth)
+    println(s"$flatNinth") // will find out how to work with enharmonics later
     val susChord = chord.sus2
     println(susChord)
     val addChord = chord.add9
@@ -30,14 +30,16 @@ object Main:
     val cRoot      = "C".note
     val thirdM     = root.transposeBy(ThirdMinorInterval)
     val fifthP     = root.transposeBy(FifthPerfectInterval)
-    val minorChord = Triad(cRoot, thirdM, fifth)
+    val minorChord = Triad(cRoot, MinorChord)
     val add2       = minorChord.add2
     println(add2)
     val c = Triad.major("C".note)
     val pop =
       Progression.fromDegrees(c, RomanDegree.I, RomanDegree.V, RomanDegree.vi, RomanDegree.IV)
     println(pop)
-    val cmaj7 = c.addExtension("B".note)
-    println(cmaj7.render) // Cmaj7B <- wip
-    println("C#")
+    val sharpEleventh = c.addExtension(SharpEleventh)
+    println(sharpEleventh)
+    val seventhMajor = Triad("C".note, SeventhMajorChord)
+    println(seventhMajor.render)
     println("C#".note.toPitch)
+    println("C".note.sharpen)
