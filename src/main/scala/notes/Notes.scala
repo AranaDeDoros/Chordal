349ada45d5350e3599fa7fc8a72a1a48077e7782
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2026-01-01 22:42:07 -0600

    added more extension methods

--- src/main/scala/notes/Notes.scala
+++ src/main/scala/notes/Notes.scala
@@ -161,14 +161,20 @@ object Note:
         throw new IllegalArgumentException(s"Invalid note: $s")
 
 extension (s: String)
-  def note: Note = Note(s)
-  def major: Chord =
+  def note: Note   = Note(s)
+  def major: Chord = M
+  def M: Chord =
     val root = Note(s)
     Triad(root, MajorChord)
-  def minor: Chord =
+  def minor: Chord = min
+  def min: Chord =
     val root = Note(s)
     Triad(root, MinorChord)
+  def Maj: Triad        = Triad(Note(s), SeventhMajorChord)
+  def dim: Triad        = Triad(Note(s), DiminishedChord)
+  def aug: Triad        = Triad(Note(s), AugmentedChord)
   def power: PowerChord = PowerChord(Note(s))
+  def pow: PowerChord   = power
   def sharpen: Note =
     s.note.transposeChromatically(ChromaticUp)
   def flatten: Note =
@@ -184,5 +190,28 @@ extension (n: Note)
       .transposeDiatonically(SecondMinorInterval)
   def major: Triad =
     Triad(n, MajorChord)
+  def M: Triad = Triad(n, MajorChord)
   def minor: Triad =
     Triad(n, MinorChord)
+  def m: Triad        = Triad(n, MinorChord)
+  def Maj: Triad      = Triad(n, SeventhMajorChord)
+  def dim: Triad      = Triad(n, DiminishedChord)
+  def aug: Triad      = Triad(n, AugmentedChord)
+  def pow: PowerChord = PowerChord(n)
+
+  /** alias for a ChromaticUp transposition
+    * @return
+    */
+  def *> : Note = n.transposeChromatically(ChromaticUp)
+
+  /** alias for a ChromaticDown transposition
+    * @return
+    */
+  def <* : Note = n.transposeChromatically(ChromaticDown)
+
+  /** alias for a diatonic transposition
+    * @param interval
+    *   used to transpose to a higher pitch pass backwards = true is you wish a lower pitch
+    * @return
+    */
+  def >>(interval: Interval): Note = n.transposeDiatonically(interval)
